efcd75deffd69ab2e01dfcbd1b355e2d1a40356b
plaetzaw <plaetzaw@gmail.com>
2021-01-13 17:34:25 -0600

    Graphiql working

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql')
+const { graphqlHTTP } = require('express-graphql')
+const schema = require("./schema/schema")
 //graphqlHTTP is just a naming convention for GraphQL
 
 const PORT = process.env.PORT || 8080;
@@ -7,6 +8,8 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use("/graphql", graphqlHTTP({ 
+    schema,
+    graphiql: true
 }))
 
 app.listen(PORT, ()=> {
